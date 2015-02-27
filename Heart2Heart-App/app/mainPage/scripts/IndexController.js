7d0710bf0e50a239359912f0d2b12cab35349b20
Lance Laughlin <lancealaughlin@gmail.com>
2015-02-27 07:57:58 -0500

    Fixed issue with heart not resetting after touchstop event

--- Heart2Heart-App/app/mainPage/scripts/IndexController.js
+++ Heart2Heart-App/app/mainPage/scripts/IndexController.js
@@ -5,8 +5,13 @@ angular
     $scope.heartTouch = document.getElementById("inv-btn");
     $scope.heart = document.getElementById("heart");
     $scope.heartImgs = ["http://cl.ly/image/2B3y2e2X1G0o/Embossed_Heart_Low.png","http://cl.ly/image/1V0N1E1A3108/Embossed_Heart_Medium.png","http://cl.ly/image/2E1U153P051r/Embossed_Heart_High.png"];
+    $scope.hiddenBtns = $("#hidden-btns li");//document.getElementsByClassName("hidden-btns");
     $scope.heartTouch.addEventListener("touchstart", startBeat, false);
     $scope.heartTouch.addEventListener("touchend", stopBeat, false);
+    //$scope.hiddenBtns.addEventListener("touchstart",toggleMode, false);
+    jQuery($scope.hiddenBtns).click(function( e ) {
+      toggleMode();// true
+    });
     $scope.timer; //For beat 1
     $scope.mode = 0; //0 = low(blue) 1 = average(green)  2 = fast (red)
 
@@ -27,7 +32,11 @@ angular
     }
 
     function stopBeat(){
-    	window.clearInterval($scope.timer);
         jQuery($scope.heart).attr("src",$scope.heartImgs[0]);
+    	window.clearInterval($scope.timer);
+    }
+
+    function toggleMode(){
+        navigator.notification.vibrate(2500);
     }
   });
