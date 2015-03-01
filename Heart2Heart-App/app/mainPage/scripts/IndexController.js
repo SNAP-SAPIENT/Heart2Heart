a38ef631d92ac056decaf0508621c1149634b38e
Lance Laughlin <lancealaughlin@gmail.com>
2015-03-01 14:01:09 -0500

    Mobile App completed

--- Heart2Heart-App/app/mainPage/scripts/IndexController.js
+++ Heart2Heart-App/app/mainPage/scripts/IndexController.js
@@ -5,7 +5,7 @@ angular
 
     //DOM Stuff
     $scope.heart = document.getElementById("heart");
-    $scope.hiddenBtns = jQuery("#hidden-btns div");
+    $scope.hiddenBtns = jQuery("#hidden-btns button");
 
     //Event Listeners
     $scope.heart.addEventListener("touchstart", startBeat, false);
@@ -23,8 +23,9 @@ angular
     //Preload images
     preload([
         'http://cl.ly/image/0P052W1i3i2D/H2H_Activity_Mode.png',
-        'http://cl.ly/image/3h341s1I121O/Red_Heart.gif',
-        'http://cl.ly/image/0B39263I0R0Y/Green_Heart.gif'
+        'http://cl.ly/image/0O2h2Q373m14/H2H_Sleep_Mode.png',
+        'http://cl.ly/image/1C290H0d0k0g/Red_Heart.gif',
+        'http://cl.ly/image/0V0b1e003N16/Green_Heart.gif'
     ]);
 
     function preload(arrayOfImages) {
@@ -35,7 +36,7 @@ angular
 
     function startBeat(){
         if($scope.mode == "green"){
-            $scope.timer = setInterval(function(){navigator.notification.vibrate(2500);},600);  
+            $scope.timer = setInterval(function(){navigator.notification.vibrate(2500);},900);  
             normalMode();
         }else if($scope.mode == "red"){
             $scope.timer = setInterval(function(){navigator.notification.vibrate(2500);},500); 
@@ -51,33 +52,27 @@ angular
     }
 
     function defaultMode(){
-        jQuery("#heart").attr("src","http://cl.ly/image/1Y0N0B1b0h3A/Default.jpg");
+        jQuery("#heart").attr("src","http://cl.ly/image/120z3A0W322c/Default.jpg");
         jQuery("#activity-notif").css("visibility","hidden");
-        jQuery("body").css("background-color","#FFFFFF");
-        jQuery("#hidden-btns div").css("border","#f8f8f8");
     }
 
     function normalMode(){
-        jQuery("#heart").attr("src","http://cl.ly/image/0B39263I0R0Y/Green_Heart.gif");
-        jQuery("body").css("background-color","#92d5a9");
+        jQuery("#heart").attr("src","http://cl.ly/image/0V0b1e003N16/Green_Heart.gif");
         jQuery("#offline-notif").css("visibility","hidden");
         jQuery("#activity-notif").css("visibility","hidden");
-        jQuery("#hidden-btns div").css("border","#92d5a9");
     }
 
     function fitnessMode(){
-        jQuery("#heart").attr("src","http://cl.ly/image/3h341s1I121O/Red_Heart.gif");
-        jQuery("body").css("background-color","#d5797a");
+        jQuery("#heart").attr("src","http://cl.ly/image/1C290H0d0k0g/Red_Heart.gif");
         jQuery("#offline-notif").css("visibility","hidden");
         jQuery("#activity-notif").css("visibility","visible");
-        jQuery("#hidden-btns div").css("border","#d5797a");
+        jQuery("#hidden-btns div").css("border","black");
 
     }
 
     function toggleMode(el){
         if(jQuery(el).attr('id') == "green"){
             $scope.mode = "green";
-            
         }else if(jQuery(el).attr('id') == "red"){
             $scope.mode = "red";
         }else if(jQuery(el).attr('id') == "offline"){
